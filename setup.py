ae35d40f84072221f7a7b84a560945444edb3ba6
jack kweyunga <75433841+jackkweyunga@users.noreply.github.com>
2022-08-27 13:59:42 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     
 setup(
     name='django-quiz-app',
-    version='0.0.1',
+    version='0.0.12',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
